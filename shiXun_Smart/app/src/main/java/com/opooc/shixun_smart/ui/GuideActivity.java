5d7524230a4aace5b83c24bd78351766bba0f3d1
opooc <admin@opooc.com>
2019-02-22 09:31:41 +0800

    2-4finished

--- shiXun_Smart/app/src/main/java/com/opooc/shixun_smart/ui/GuideActivity.java
+++ shiXun_Smart/app/src/main/java/com/opooc/shixun_smart/ui/GuideActivity.java
@@ -1,5 +1,6 @@
 package com.opooc.shixun_smart.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
@@ -7,8 +8,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.opooc.shixun_smart.MainActivity;
 import com.opooc.shixun_smart.R;
+import com.opooc.shixun_smart.utils.L;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +25,16 @@ import java.util.List;
  *  Creat_name:  2019/2/21 11:14 PM
  *  note:        引导页
  */
-public class GuideActivity extends AppCompatActivity {
+public class GuideActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewPager mViewPager;
     //容器
     private List<View> mList = new ArrayList<>();
     private View view1,view2,view3;
-
+    //小圆点
+    private ImageView point1,point2,point3;
+    //跳过
+    private ImageView iv_back;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,17 +45,74 @@ public class GuideActivity extends AppCompatActivity {
     }
 
     private void initView() {
+
+        iv_back = (ImageView)findViewById(R.id.iv_back);
+        iv_back.setOnClickListener(this);
+
+
+        point1 = (ImageView) findViewById(R.id.point1);
+        point2 = (ImageView) findViewById(R.id.point2);
+        point3 = (ImageView) findViewById(R.id.point3);
+
+        setPointImg(true,false,false);
+
         mViewPager = (ViewPager) findViewById(R.id.mViewPager);
         view1 = View.inflate(this,R.layout.page_item_one,null);
         view2 = View.inflate(this,R.layout.page_item_two,null);
         view3 = View.inflate(this,R.layout.page_item_three,null);
 
+
+        view3.findViewById(R.id.btn_start).setOnClickListener(this);
+
         mList.add(view1);
         mList.add(view2);
         mList.add(view3);
-
+        //设置适配器
         mViewPager.setAdapter(new GuideAdapter());
+        //监听VIewpager
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                L.i("position:"+ position);
+                switch (position){
+                    case 0:
+                        setPointImg(true,false,false);
+                        iv_back.setVisibility(View.VISIBLE);
+                        break;
+                    case 1:setPointImg(false,true,false);
+                        iv_back.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:setPointImg(false,false,true);
+                        iv_back.setVisibility(View.INVISIBLE);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_back:
+            case R.id.btn_start:
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+
+        }
     }
 
 
@@ -72,6 +136,24 @@ public class GuideActivity extends AppCompatActivity {
         public void destroyItem(ViewGroup container, int position, Object object) {
             ((ViewPager)container).removeView(mList.get(position));
 //            super.destroyItem(container, position, object);
+        }
+    }
+    //设置小圆点的选定参数
+    private  void setPointImg(boolean isCheck1,boolean isCheck2,boolean isCheck3){
+        if (isCheck1){
+            point1.setBackgroundResource(R.drawable.point_on);
+        }else {
+            point1.setBackgroundResource(R.drawable.point_off);
+        }
+        if (isCheck2){
+            point2.setBackgroundResource(R.drawable.point_on);
+        }else {
+            point2.setBackgroundResource(R.drawable.point_off);
+        }
+        if (isCheck3){
+            point3.setBackgroundResource(R.drawable.point_on);
+        }else {
+            point3.setBackgroundResource(R.drawable.point_off);
         }
     }
 }
